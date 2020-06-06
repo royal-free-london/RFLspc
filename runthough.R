dee3f1f153a9f3dd6e6913c4d75b6dd75b55afe6
Edward Watkinson <ed@edwatto.com>
2020-06-06 14:24:29 +0100

    create summary function ggRFLspc and allow other functions to be piped (output & input working file location as main/first parameter)

--- runthough.R
+++ runthough.R
@@ -11,7 +11,7 @@ library(lubridate)
 library(openxlsx)
 library(RFLtheme)
 library(RDCOMClient) # Have to install.packages("RDCOMClient", repos = "http://www.omegahat.net/R")
-
+library(tictoc)
 ####----Variables----####
 # tab - master
 # input
@@ -21,11 +21,12 @@ library(RDCOMClient) # Have to install.packages("RDCOMClient", repos = "http://w
 # dates = b26-284
 # data = c26-284
 vtemplateFile <- 'SPC Reporting Template.xlsx'
-vworkingFile <- 'working.xlsx'
+vworkingFile <- paste(getwd(), 'working.xlsx', sep= '/')
 
 ####----Data Access----####
 df_exampleData <- read.csv('example_data.csv') %>%
   mutate(Date.Time = dmy(Date.Time))
+
 
 ####----Input Options & Data----####
 
@@ -33,29 +34,31 @@ rfl_load_spc_model_input <- function(data, templateFile, workingFile) {
   wb <- openxlsx::loadWorkbook(templateFile)
   writeData(wb, 1, data,  startRow = 26, startCol = 2,
             colNames= FALSE)
-  saveWorkbook(wb, workingFile, overwrite = TRUE)  
+  saveWorkbook(wb, workingFile, overwrite = TRUE) 
+  
+  return(workingFile)
 }
 
-rfl_load_spc_model_input(df_exampleData, vtemplateFile, vworkingFile)
+# rfl_load_spc_model_input(df_exampleData, vtemplateFile, vworkingFile)
 
 
 ####----Run SPC Excel Model----####
 
-rfl_calculate_wb <- function(outputfile) {
-  library(RDCOMClient)
-  
+rfl_calculate_wb <- function(workingFile) {
   # Create COM Connection to Excel
   xlApp <- COMCreate("Excel.Application")
   xlApp[['Visible']] <- FALSE
   xlApp[['DisplayAlerts']] <- FALSE
   
   # Open workbook
-  xlWB <- xlApp[["Workbooks"]]$Open(outputfile)
+  xlWB <- xlApp[["Workbooks"]]$Open(workingFile)
   xlWB$Save()
   xlWB$Close(TRUE) 
+  
+  return(workingFile)
 }
 
-rfl_calculate_wb(paste(getwd(), vworkingFile, sep='/'))
+# rfl_calculate_wb(paste(getwd(), workingFile, sep='/'))
 
 ####----Output Data for Chartting----####
 # Get file
@@ -65,17 +68,18 @@ rfl_get_spc_model_output <- function(workingFile) {
                          detectDates = TRUE,
                          check.names = TRUE)
   
+  # Remove working file
+  file.remove(workingFile)
+  
   # Filter for non-empty rows and important columns
   df_output %>%
     filter(!is.na(Date.Time)) %>%
     select(`Date.Time`, Value, Trajectory, Goal, Mean, Upper, Lower, Shift=Shift.1, Trend=Trend.1, Outlier) %>%
     mutate()
   
-  # Remove working file
-  file.remove(workingFile)
 }
 
-rfl_get_spc_model_output(vworkingFile)
+# df_output_tidy <- rfl_get_spc_model_output(vworkingFile)
 
 ####----Chart Data----####
 
@@ -96,4 +100,17 @@ rfl_spc_plot <- function(data, pointSize = 2, ...) {
     rfl_style()
 }
 
-rfl_spc_plot(df_output_tidy, title = "Example SPC Chart", subtitle = "Here is a subtitle")
+# rfl_spc_plot(df_output_tidy, title = "Example SPC Chart", subtitle = "Here is a subtitle")
+
+
+ggRFLspc <- function(data, templateFile, workingFile, ...) {
+  df_exampleData %>%
+    rfl_load_spc_model_input(templateFile, workingFile) %>%
+    rfl_calculate_wb %>%
+    rfl_get_spc_model_output %>%
+    rfl_spc_plot(...)
+}
+
+tic()
+ggRFLspc(df_exampleData, vtemplateFile, vworkingFile, title = "Example SPC Chart", subtitle = "Here is a subtitle")
+toc()
